7cf14c24757d744eb0fe86cabb89b4c3504ce4e3
walkthunder Cooper <walkthunder@163.com>
2023-08-06 18:17:02 +0800

    fix(bump): Fix script error.

--- bump.mjs
+++ bump.mjs
@@ -8,9 +8,9 @@ console.log(`Bumping version to ${targetVersion}`)
 const manifest = JSON.parse(readFileSync('manifest.json', 'utf8'))
 const { minAppVersion } = manifest
 manifest.version = targetVersion
-writeFileSync('manifest.json', JSON.stringify(manifest, null, '4'))
+writeFileSync('manifest.json', JSON.stringify(manifest, null, 4))
 
 // update versions.json with target version and minAppVersion from manifest.json
 const versions = JSON.parse(readFileSync('versions.json', 'utf8'))
 versions[targetVersion] = minAppVersion
-writeFileSync('versions.json', JSON.stringify(versions, null, '4'))
+writeFileSync('versions.json', JSON.stringify(versions, null, 4))
